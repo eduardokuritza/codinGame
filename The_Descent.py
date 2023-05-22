419789e1f4198053859dec6c598bd000758bd8c2
Eduardo Kuritza <eduardo.kuritza@gmail.com>
2023-05-22 16:58:39 -0300

    resolution

---
+++ The_Descent.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+while True:
+    x = 0
+    y = 0
+
+    for i in range(8):
+        mountain_h = int(input())
+        if mountain_h > x: 
+            x = mountain_h
+            y = i
+
+    print(y)
